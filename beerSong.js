c7bee8926cf2115ffae5d552b9b562a72872b562
Parichat-amp <123495971+Parichat-amp@users.noreply.github.com>
2023-11-03 19:09:05 +0100

    Create beerSong.js

---
+++ beerSong.js
@@ -0,0 +1,27 @@
+var bottlesOfBeer = 99;
+
+while (bottlesOfBeer >= 0) {
+  var wordBottle = "bottles";
+
+  if (bottlesOfBeer === 1) {
+    wordBottle = "bottle";
+  }
+
+  console.log(bottlesOfBeer + " " + wordBottle + " of beer on the wall,");
+  console.log(bottlesOfBeer + " " + wordBottle + " of beer.");
+  console.log("Take one down and pass it around,");
+
+  bottlesOfBeer--;
+
+  if (bottlesOfBeer === 1) {
+    wordBottle = "bottle";
+  } else if (bottlesOfBeer === 0) {
+    wordBottle = "bottles";
+    console.log("No more bottles of beer on the wall,");
+    console.log("No more bottles of beer.");
+    console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+    break; // Exit the loop
+  }
+
+  console.log(bottlesOfBeer + " " + wordBottle + " of beer on the wall.");
+}
